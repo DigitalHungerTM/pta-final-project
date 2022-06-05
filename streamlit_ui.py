047fa810cbd0c766f8bfe23b2e14f3e27911f833
Thijmen Adam <thijmenadam@gmail.com>
2022-06-05 14:04:38 +0200

    updates streamlit_ui to run on an uploaded file instead of sys.argv

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,5 +1,10 @@
+# filename: wikificator.py
+# author: Mathijs Afman, Maxim van der Maesen de Sombreff, Thijmen Adam
+# description: Takes a wikificated file and shows it with clickable links.
+# usage: streamlit run streamlit_ui.py
+# date: 05-06-2022
+
 import streamlit as st
-import os
 import sys
 
 def open_raw_file(filename):
@@ -10,31 +15,37 @@ def open_raw_file(filename):
 
 def main():
 
-    path = os.getcwd()
-    
-    target_path = path + sys.argv[1] + "/en.tok.off.pos.aut"
+    # target_path = path + sys.argv[1] + "/en.tok.off.pos.aut"
 
-    raw_text = open_raw_file(target_path)
+    target_input = st.file_uploader("Please upload a .aut file here", type=["aut"])
 
-    lines = raw_text.split("\n")
+    if target_input != None:
+        # raw_text = raw_file.read()
+        target_path = target_input.read().decode()
 
-    line_list = []
+        lines = target_path.split("\n")
 
-    for line in lines:
-        new_line = line.split(" ")
-        line_list.append(new_line)
+        line_list = []
 
-    print_string = ""
+        for line in lines:
+            new_line = line.split(" ")
+            line_list.append(new_line)
 
-    for item in line_list:
-        if len(item) > 4:
-            if len(item) > 5:
-                if "https" in item[6]:
-                    print_string += " ["+ item[3] +"]("+ item[6]+ ")"
-            else:
-                print_string += " " + item[3]
+        print_string = ""
 
-    st.write(print_string)
+        for item in line_list:
+            if len(item) > 4:
+                if len(item) > 5:
+                    if "https" in item[6]:
+                        print_string += " ["+ item[3] +"]("+ item[6]+ ")"
+                else:
+                    print_string += " " + item[3]
+
+        st.write(print_string)
+
+    else:
+        st.write("You should upload a .aut file, created by the wikificator.py"
+                 " file that is included with the files of this project.")
 
 if __name__ == "__main__":
     main()
